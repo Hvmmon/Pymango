6d11165f19dd7b7e4404e05466e91e303bb2babf
Hvmmon <kkan@apcs.vn>
2022-09-13 00:50:47 +0700

    Add extra trees of ml algorithm, ab tests and view pages for ab tests.

--- pymango/pymango/wsgi.py
+++ pymango/pymango/wsgi.py
@@ -19,6 +19,7 @@ application = get_wsgi_application()
 import inspect
 from apps.mlr.registry import MLRegistry
 from apps.mlr.income_classifier.random_forest import RandomForestClassifier
+from apps.mlr.income_classifier.extra_trees import ExtraTreesClassifier
 
 try:
     registry = MLRegistry() # create ML registry
@@ -32,6 +33,17 @@ try:
                             algorithm_version="0.0.2",
                             owner="An",
                             algorithm_description="Random Forest with simple pre- and post-processing",
+                            algorithm_code=inspect.getsource(RandomForestClassifier))
+    # Extra Trees classifier
+    et = ExtraTreesClassifier()
+    # add to ML registry
+    registry.add_algorithm(endpoint_name="income_classifier",
+                            algorithm_object=et,
+                            algorithm_name="extra trees",
+                            algorithm_status="testing",
+                            algorithm_version="0.0.1",
+                            owner="Piotr",
+                            algorithm_description="Extra Trees with simple pre- and post-processing",
                             algorithm_code=inspect.getsource(RandomForestClassifier))
 
 except Exception as e:
